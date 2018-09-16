fa4598f485d87002013fc1bf28686c91431777cf
Michael Weller <vve11ermichae1@gmail.com>
2018-09-16 17:38:35 +0100

    Code simplification and section and header added in html

--- js/app.js
+++ js/app.js
@@ -6,41 +6,14 @@ $(document).ready(function() {
   const frameWidth = 850; // Width of the game frame.
 
   // Meteor class used to handle falling meteors and their behaviours.
-  class Meteor {
-    constructor() {
-      switch (Math.ceil(Math.random()*3)) {
-        case 1:
-        this.direction = "down";
-        break;
-
-        case 2:
-        this.direction = "left";
-        break;
-
-        case 3:
-        this.direction = "right";
-        break;
-      }
-      this.spawnMeteor();
-    }
+  function Meteor() {
 
     // Function that spawns the meteor at a random X coordinate at a fixed Y coordinate.
-    spawnMeteor() {
-      var spawnX;
-      switch (this.direction) {
-        case "down":
-          spawnX = Math.ceil(Math.random()*frameWidth);
-          break;
-        case "left":
-          spawnX = (frameWidth / 2) + Math.ceil(Math.random()*(frameWidth/2));
-          break;
-        case "right":
-          spawnX = Math.ceil(Math.random()*(frameWidth/2));
-          break;
-      }
-
-      // Add the meteor to the frame.
-      gameFrame.html("<div class=\"meteor-" + this.direction + "\" style='top:" + spawnY + ";left:" + spawnX + "px;'> </div>");
+    this.spawnMeteor = function() {
+      var spawnX = Math.ceil(Math.random()*(frameWidth-80));
+      gameFrame.append("<div class='meteor' style='top:" + spawnY + ";left:" + spawnX + "px;'> </div>");
     }
+
+    this.spawnMeteor();
   }
 });

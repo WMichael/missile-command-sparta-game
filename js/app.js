8a20f8d5d5ffbe14a221b42c2728b3b490e0df4b
Michael Weller <vve11ermichae1@gmail.com>
2018-09-17 10:09:40 +0100

    Added destroy meteor on click event listener

--- js/app.js
+++ js/app.js
@@ -7,36 +7,42 @@ $(document).ready(function() {
 
   // Meteor class used to handle falling meteors and their behaviours.
   function Meteor(id) {
-    this.id = id;
-    this.meteor;
+    this.id = id; // Id attached to the Meteor element
+    var meteorElement; // Had to use a var rather than this variable.
 
     // Function that spawns the meteor at a random X coordinate at a fixed Y coordinate.
     this.spawnMeteor = function() {
       var spawnX = Math.ceil(Math.random()*(frameWidth-180));
       gameFrame.append("<div id='" + id + "' class='meteor' style='top:" + spawnY + "px;left:" + spawnX + "px;'> </div>");
-      this.meteor = $("#" + id);
+      meteorElement = $("#" + id);
     }
 
     // Function for dropping the meteor
     this.fall = function() {
       var speed = Math.ceil(Math.random()*5);
       var currentY = spawnY;
-      var meteor = this.meteor; // Helper variable used so that this.meteor can be used in the setInterval function.
       setInterval(function() {
         if(currentY <= (frameHeight - 42)) {
-          meteor.css("top",currentY + "px");
+          meteorElement.css("top",currentY + "px");
           currentY += speed;
         }
       },100);
     }
 
+    // Removes element on click
+    this.destroy = function() {
+      meteorElement.remove();
+    }
+
     this.spawnMeteor();
+    meteorElement.click(this.destroy); // Event listener for the object
     this.fall();
   }
 
   // Testing the meteors
   for (var i = 0; i < 5; i++) {
     meteor = new Meteor(i);
+    console.log(meteor);
   }
 
 });
